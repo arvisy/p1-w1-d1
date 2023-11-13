f0c989ae84c23f10ff2bd12e2f3fd2c0bb32cf9d
arvisy <arviansyahnur3@gmail.com>
2023-11-13 10:43:51 +0700

    add main.go

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!");
+	fmt.Println("Motivasi belajar golang karena digunakan oleh beberapa perusahaan terkemuka seperti Google, Dropbox, Uber, dan lainnya. Memahami Golang dapat membuka peluang karier yang lebih luas dan memberikan keunggulan dalam dunia profesional.");
+}
